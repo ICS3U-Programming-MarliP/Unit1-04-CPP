31196d143d128ce144b66f881eb8596d31e9662d
Marli Peters <marley.peters@stu.ocsb.ca>
2023-09-19 22:52:58 +0000

    Commited CPP Math to Github

---
+++ areaPerRect.cpp
@@ -0,0 +1,14 @@
+// Copyright 2023 Marli Peters
+// Created By: Marli Peters
+// Date: Sep. 19, 2023
+// This program does basic math.
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions:" << std::endl;
+    std::cout << "5cm x 3cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is: " << (5 * 3) << "cm^2" << std::endl;
+    std::cout << "Perimeter is: " << (2 * (5 + 3)) << "cm" << std::endl;
+}
---
+++ basicMath.cpp
@@ -0,0 +1,17 @@
+// Copyright 2023 Marli Peters
+// Created By: Marli Peters
+// Date: Sep. 19, 2023
+// This program does basic math.
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "9 + 2 = " << 9 + 2 << std::endl;
+    std::cout << "7 - 3 = " << 7 - 3 << std::endl;
+    std::cout << "10/2 = " << 10 / 2 << std::endl;
+    std::cout << "8 - 4 / 2 = " << 8 - 4 / 2 << std::endl;
+    std::cout << "9 + 2 = " << 9 + 2 << std::endl;
+    std::cout << "2^3 = " << pow(2, 3) << std::endl;
+    std::cout << "9 + 2 = " << 9 + 2 << std::endl;
+    std::cout << "1 / 3 = " << float(1) / float(3) << std::endl;
+}
